428306f7842b8b901ce3294f63f3510ff559810e
Harsh Kumar <harsh.kumar@boeing.com>
2022-03-12 21:15:52 +0530

    Second Comitt

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IProperty } from '../IProperty.interface';
 
 @Component({
   selector: 'app-property-card',
   templateUrl: './property-card.component.html',
   styleUrls: ['./property-card.component.css']
 })
-export class PropertyCardComponent implements OnInit {
 
-  constructor() { }
+export class PropertyCardComponent  {
 
-  ngOnInit(): void {
-  }
+  @Input() property : any
 
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+export class PropertyListComponent implements OnInit {
+  properties: any ;
+  constructor(private housingservice :HousingService) { }
 
   ngOnInit(): void {
+    this.housingservice.getAllProperties().subscribe(
+      data =>{
+        this.properties =data;
+      },
+      error =>{
+            console.log(error)
+      }
+      );
   }
 
 }
